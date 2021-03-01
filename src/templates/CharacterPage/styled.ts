1f668ce71e51e68caf236e61e4f50b4d171e753d
Junior Alves <jjalves96@gmail.com>
2021-03-01 08:40:27 -0300

    chore: add new styles into component

--- src/templates/CharacterPage/styled.ts
+++ src/templates/CharacterPage/styled.ts
@@ -1,6 +1,4 @@
 import styled, { css } from 'styled-components';
-import { Form } from '@unform/web';
-import { motion } from 'framer-motion';
 
 export const Container = styled.div`
   width: 100%;
@@ -8,7 +6,37 @@ export const Container = styled.div`
 `;
 
 export const Content = styled.div`
-  margin: 0 auto;
-  width: 100%;
-  max-width: 960px;
+  ${({ theme }) => css`
+    margin: 0 auto;
+    width: 100%;
+    max-width: 960px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 2.4rem;
+
+    p {
+      font-size: ${theme.font.sizes.xlarge};
+      font-weight: ${theme.font.bold};
+      margin-left: 3rem;
+    }
+
+    img {
+      border-radius: 8px;
+      overflow: hidden;
+      box-shadow: ${theme.box.shadow};
+    }
+
+    @media (max-width: 768px) {
+      display: flex;
+      flex-direction: column;
+
+      p {
+        font-size: ${theme.font.sizes.xlarge};
+        font-weight: ${theme.font.bold};
+        margin-top: 2.4rem;
+      }
+    }
+  `}
 `;
