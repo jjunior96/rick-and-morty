6ba0082fd6a66e5bfe0e3d0c864cb53e1685af2b
Junior Alves <jjalves96@gmail.com>
2021-03-03 16:26:35 -0300

    chore: add motion into component

--- src/templates/CharacterPage/index.tsx
+++ src/templates/CharacterPage/index.tsx
@@ -1,9 +1,18 @@
+import { IoArrowBackOutline } from 'react-icons/io5';
+
 import * as S from './styled';
 
 const CharacterPage: React.FC = ({ children }) => {
   return (
     <S.Container>
-      <S.Content>{children}</S.Content>
+      <S.Content>
+        <S.Header>
+          <S.BackButton href="/">
+            <IoArrowBackOutline style={{ cursor: 'pointer' }} />
+          </S.BackButton>
+        </S.Header>
+        {children}
+      </S.Content>
     </S.Container>
   );
 };
