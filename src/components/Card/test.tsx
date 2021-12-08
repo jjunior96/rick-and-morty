40968aac240607ab2f7425d7ee5a445ad691293f
JUNIOR ALVES <jjalves96@gmail.com>
2021-12-08 08:51:38 -0300

    chore: add changes on Card component

--- src/components/Card/test.tsx
+++ src/components/Card/test.tsx
@@ -1,13 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+
+import { renderWithTheme } from 'utils/tests/helpers';
 
 import Card from '.';
 
 describe('<Card />', () => {
   it('should render the heading', () => {
-    const { container } = render(<Card />);
+    const { container } = renderWithTheme(
+      <Card title="title" img="/img/logo.svg" />
+    );
 
     expect(
-      screen.getByRole('heading', { name: /Card/i })
+      screen.getByRole('img', { name: /thumbnail of title/i })
     ).toBeInTheDocument();
 
     expect(container.firstChild).toMatchSnapshot();
